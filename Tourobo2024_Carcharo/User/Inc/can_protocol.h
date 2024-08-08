6a8a249ed1d6fdaee4c036cd5ed01294be84b6d4
Clacks5 <haruto0921vi@gmail.com>
2024-08-08 14:37:09 +0900

    refactor: robomaster.hとrobomaster.cをUser/に追加してmain.cを簡素化

---
+++ Tourobo2024_Carcharo/User/Inc/can_protocol.h
@@ -0,0 +1,22 @@
+/*
+ * can_protocol.h
+ *
+ *  Created on: Jul 17, 2024
+ *      Author: Haruto HAGIWARA
+ */
+
+#ifndef INC_CAN_PROTOCOL_H_
+#define INC_CAN_PROTOCOL_H_
+
+#include "stm32f7xx_hal.h"
+
+typedef enum{
+	ROBOMASTER_ID = 0x200,
+	SHOKI_ID = 0x300,
+} CAN_ID;
+
+typedef enum{
+
+};
+
+#endif /* INC_CAN_PROTOCOL_H_ */
