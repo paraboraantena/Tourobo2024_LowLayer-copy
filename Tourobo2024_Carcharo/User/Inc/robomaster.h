e6e6a4f25e11b827989b9f7f47d3950dc21a8352
yod2004 <yodai0903@gmail.com>
2024-08-29 19:49:40 +0900

    未追跡ファイル全て追加

---
+++ Tourobo2024_Carcharo/User/Inc/robomaster.h
@@ -0,0 +1,48 @@
+/*
+ * robomaster.h
+ *
+ *  Created on: Jul 13, 2024
+ *      Author: Haruto HAGIWARA
+ */
+
+#ifndef INC_ROBOMASTER_H_
+#define INC_ROBOMASTER_H_
+
+#include "stm32f7xx_hal.h"
+#include "arm_math.h"
+
+typedef struct {
+	// Input torque target
+	int16_t TargetTorque;
+	// Current angle
+	int16_t Angle;
+	// Current angular velocity
+	int16_t AngularVelocity;
+	// Input torque feedback
+	int16_t FeedbackTorque;
+	// Motor Temperature
+	uint8_t MotorTemperature;
+
+	// Update Check
+	uint32_t Event;
+
+	// Encoder FeedBack
+	float32_t EncoderAngularVelocity;
+
+	// Target Angular Velocity
+	float32_t TargetAngularVelocity;
+	float32_t PreTargetAngularVelocity;
+	// Angular Velocity Error
+	float32_t AngularVelocityError;
+	// PID instance
+	/*
+	 * C(z) = Kp + (1 - z^{-1}) * Kd + 1 / (1 - z^{-1}) * Ki
+	 */
+	arm_pid_instance_f32 PID;
+} RobomasterTypedef;
+
+void Robomaster_InitZero(RobomasterTypedef *Robomaster);
+
+void Robomaster_RxCAN(RobomasterTypedef *Robomaster, uint8_t *RxData);
+
+#endif /* INC_ROBOMASTER_H_ */
