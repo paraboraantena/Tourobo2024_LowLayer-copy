400f51c826c928ed089edc2258c6135729a6d42f
Matsumoto-Rena <m-rena.0707@outlook.jp>
2024-09-01 20:22:56 +0900

    CAN

--- Tourobo2024_Uator-ws2812b/Core/Src/main.c
+++ Tourobo2024_Uator-ws2812b/Core/Src/main.c
@@ -31,7 +31,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define LED_NUMBER 10
+#define LED_NUMBER 284
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -42,10 +42,24 @@
 /* Private variables ---------------------------------------------------------*/
 CAN_HandleTypeDef hcan2;
 
+SPI_HandleTypeDef hspi1;
 SPI_HandleTypeDef hspi2;
 
 /* USER CODE BEGIN PV */
+//for can
+uint32_t id;
+uint32_t dlc;
+uint8_t data[8];
+uint8_t failure = 0;
+
+//for solenoid
+uint8_t solenoid[6] = {};
+
+//for LED
 uint8_t LED_Data[3] = {};
+
+GPIO_TypeDef* solv_ports[6] = {SOLV1_GPIO_Port, SOLV2_GPIO_Port, SOLV3_GPIO_Port, SOLV4_GPIO_Port, SOLV5_GPIO_Port, SOLV6_GPIO_Port};
+uint16_t solv_pins[6] = {SOLV1_Pin, SOLV2_Pin, SOLV3_Pin, SOLV4_Pin, SOLV5_Pin, SOLV6_Pin};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -53,13 +67,112 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_CAN2_Init(void);
 static void MX_SPI2_Init(void);
+static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
 void setLED(int, int, int);
 void ws2812b_send(int, int, int);
+void ws2812b_send_green (void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan2) {
+	CAN_RxHeaderTypeDef RxHeader;
+	uint8_t RxData[2];
+	if (HAL_CAN_GetRxMessage(hcan2, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK) {
+		if(RxHeader.StdId == 0x201){
+
+			id = (RxHeader.IDE == CAN_ID_STD) ? RxHeader.StdId : RxHeader.ExtId;   // ID
+			dlc = RxHeader.DLC;                                                   // DLC
+			data[0] = RxData[0];                                                 // Data
+			data[1] = RxData[1];
+
+			for(int i=0; i<6; i++){
+				solenoid[i] = (data[0] >> (5-i)) | 0x01;
+			}
+
+			for(int i=0; i<6; i++){
+				switch(solenoid[i]){
+				case 1:
+					HAL_GPIO_WritePin(solv_ports[i], solv_pins[i], GPIO_PIN_SET);
+					break;
+
+				case 2:
+					HAL_GPIO_WritePin(solv_ports[i], solv_pins[i], GPIO_PIN_RESET);
+					break;
+				}
+			}
+
+			for(int i=0; i<10; i++){
+				switch(data[1]){
+
+				case 0:
+					LED_Data[0] = 0;
+					LED_Data[1] = 255;
+					LED_Data[2] = 0;
+					break;
+
+				case 1:
+					LED_Data[0] = 127;
+					LED_Data[1] = 255;
+					LED_Data[2] = 0;
+					break;
+
+				case 2:
+					LED_Data[0] = 255;
+					LED_Data[1] = 255;
+					LED_Data[2] = 0;
+					break;
+
+				case 3:
+					LED_Data[0] = 255;
+					LED_Data[1] = 127;
+					LED_Data[2] = 0;
+					break;
+
+				case 4:
+					LED_Data[0] = 255;
+					LED_Data[1] = 0;
+					LED_Data[2] = 127;
+					break;
+
+				case 5:
+					LED_Data[0] = 255;
+					LED_Data[1] = 0;
+					LED_Data[2] = 255;
+					break;
+
+				case 6:
+					LED_Data[0] = 0;
+					LED_Data[1] = 0;
+					LED_Data[2] = 255;
+					break;
+
+				case 7:
+					LED_Data[0] = 0;
+					LED_Data[1] = 127;
+					LED_Data[2] = 255;
+					break;
+
+				case 8:
+					LED_Data[0] = 0;
+					LED_Data[1] = 255;
+					LED_Data[2] = 255;
+					break;
+
+				case 9:
+					LED_Data[0] = 0;
+					LED_Data[1] = 255;
+					LED_Data[2] = 127;
+					break;
+
+				}
+			}
+		}
+
+	}
+}
+
 
 /* USER CODE END 0 */
 
@@ -94,8 +207,28 @@ int main(void)
   MX_GPIO_Init();
   MX_CAN2_Init();
   MX_SPI2_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
+	CAN_FilterTypeDef filter;
+	uint32_t fId1 = 0x201 << 21; // フィルターID1
+	uint32_t fId2 = 0x0 << 21;  // フィルターID2
 
+	filter.FilterIdHigh         = fId1 >> 16;            // フィルターID1の上�?16ビッ�?
+	filter.FilterIdLow          = fId1;                  // フィルターID1の下�?16ビッ�?
+	filter.FilterMaskIdHigh     = fId2 >> 16;            // フィルターID2の上�?16ビッ�?
+	filter.FilterMaskIdLow      = fId2;                  // フィルターID2の下�?16ビッ�?
+	filter.FilterScale          = CAN_FILTERSCALE_32BIT; // 32モー�?
+	filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;      // FIFO0へ格�?
+	filter.FilterBank           = 0;
+	filter.FilterMode           = CAN_FILTERMODE_IDLIST; // IDリストモー�?
+	filter.SlaveStartFilterBank = 14;
+	filter.FilterActivation     = ENABLE;
+
+	HAL_CAN_ConfigFilter(&hcan2, &filter);
+	// CANスター ?
+	HAL_CAN_Start(&hcan2);
+	// 割り込み有効
+	HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO0_MSG_PENDING);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -103,34 +236,8 @@ int main(void)
   while (1)
   {
 	  HAL_Delay(1000);
-
-	  setLED(0,255,0);
 	  ws2812b_send(LED_Data[0],LED_Data[1],LED_Data[2]);
-	  HAL_Delay(1000);
-
-	  setLED(165,255,0);
-	  ws2812b_send(LED_Data[0],LED_Data[1],LED_Data[2]);
-	  HAL_Delay(1000);
-
-	  setLED(255,255,0);
-	  ws2812b_send(LED_Data[0],LED_Data[1],LED_Data[2]);
-	  HAL_Delay(1000);
-
-	  setLED(255,0,0);
-	  ws2812b_send(LED_Data[0],LED_Data[1],LED_Data[2]);
-	  HAL_Delay(1000);
-
-	  setLED(255,0,255);
-	  ws2812b_send(LED_Data[0],LED_Data[1],LED_Data[2]);
-	  HAL_Delay(1000);
-
-	  setLED(0,0,255);
-	  ws2812b_send(LED_Data[0],LED_Data[1],LED_Data[2]);
-	  HAL_Delay(1000);
-
-	  setLED(0,255,255);
-	  ws2812b_send(LED_Data[0],LED_Data[1],LED_Data[2]);
-	  HAL_Delay(1000);
+	  ws2812b_send_green ();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -222,6 +329,44 @@ static void MX_CAN2_Init(void)
 }
 
 /**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
   * @brief SPI2 Initialization Function
   * @param None
   * @retval None
@@ -291,11 +436,11 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : ENC4A_Pin ENC4B_Pin */
-  GPIO_InitStruct.Pin = ENC4A_Pin|ENC4B_Pin;
+  /*Configure GPIO pin : ENC4A_Pin */
+  GPIO_InitStruct.Pin = ENC4A_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(ENC4A_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : SOLV6_Pin SOLV5_Pin */
   GPIO_InitStruct.Pin = SOLV6_Pin|SOLV5_Pin;
@@ -345,13 +490,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void setLED (int GREEN, int RED, int BLUE)
-{
-	LED_Data[0] = GREEN;
-	LED_Data[1] = RED;
-	LED_Data[2] = BLUE;
-}
-
 void ws2812b_send (int GREEN, int RED, int BLUE)
 {
 	uint32_t color = GREEN<<16 | RED<<8 | BLUE;
@@ -369,6 +507,25 @@ void ws2812b_send (int GREEN, int RED, int BLUE)
 	}
 
 	HAL_SPI_Transmit(&hspi2, sendData, 24*LED_NUMBER, 1000);
+}
+
+void ws2812b_send_green ()
+{
+	uint32_t color = 255<<16 | 0<<8 | 0;
+	uint8_t sendData[24*LED_NUMBER];
+
+	for(int i=0; i<LED_NUMBER; i++){
+		for(int j=0; j<24; j++){
+			if (((color>>(23-j))&0x01) == 1) {
+				sendData[i*24 + j] = 0b11111000;  // store 1
+			}
+			else{
+				sendData[i*24 + j] = 0b11000000;  // store 0
+			}
+		}
+	}
+
+	HAL_SPI_Transmit(&hspi1, sendData, 24*LED_NUMBER, 1000);
 }
 /* USER CODE END 4 */
 
